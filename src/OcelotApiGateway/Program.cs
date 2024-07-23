065732f8558456092b551a9f640784944ff907c0
DzmitryHalubovich <buxarik493@gmail.com>
2024-07-23 17:28:35 +0300

    Add dockerfile

--- src/OcelotApiGateway/Program.cs
+++ src/OcelotApiGateway/Program.cs
@@ -1,24 +1,8 @@
-using HealthChecks.UI.Client;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
-using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using OcelotApiGateway.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
-
-/*builder.Services.AddHealthChecks()
-        .AddCheck("self", () => HealthCheckResult.Healthy())
-        .AddCheck<OfficeApiHealthCheck>("Office service");
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddHttpClient();*/
 
 builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true);
 
@@ -40,13 +24,6 @@ var app = builder.Build();
 
 app.UseRouting();
 app.UseHttpsRedirection();
-
-/*app.MapHealthChecks("/_health", new HealthCheckOptions
-{
-    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-});*/
-
-//app.MapControllers();
 
 await app.UseOcelot();
 
